74b27ca4c2463a00d8a6e741871c958fb5784c93
Tatiana443 <tvb399@yandex.ru>
2023-03-16 18:33:47 +0600

    Added Task_2

--- 1.js
+++ 1.js
@@ -2,6 +2,6 @@
 // преобразовать введенное пользователем значение в градусах по Фаренгейту
 // и вывести в alert сообщение: Цельсий: 21, Фаренгейт: 69.8
 
-const celsius = Number.parseInt(prompt("Введите температуру в градусах Цельсия"));
+const celsius = Number.parseInt(prompt("Введите температуру в градусах Цельсия:"));
 const fahrenheit = Math.floor((celsius * 9 / 5 + 32) * 100) / 100;
 alert(`Цельсий: ${celsius}, Фаренгейт: ${fahrenheit}`);
--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
 function greeting(name){
-      console.log("Hello, " + name);
+    console.log("Hello, " + name);
 }
 const name1 = prompt("Введите имя");
 greeting(name1);
